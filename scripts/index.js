0874169dfe61169a74483789a6f7818d2a61abd3
Natalia Linkova <linkova1109@yandex.ru>
2021-10-18 10:50:44 +1000

    point 5

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,33 @@
 //Popups modal windows
 
-let editProfileModal = document.querySelector('.popup_type_edit-profile');
+const editProfileModal = document.querySelector('.popup_type_edit-profile');
 const addCardModal = document.querySelector('.popup_type_add-card');
 const imageModal = document.querySelector('.popup_type_image');
 
 
 
-let editForm = editProfileModal.querySelector('.popup__form');
-let addCardForm = addCardModal.querySelector('.popup__form');
+const editForm = editProfileModal.querySelector('.popup__form');
+const addCardForm = addCardModal.querySelector('.popup__form');
 
 
 //Buttons
-let editProfileOpenButton = document.querySelector('.profile__edit-button');
+const editProfileOpenButton = document.querySelector('.profile__edit-button');
 const addCardOpenButton = document.querySelector('.profile__add-button');
 
-let addCardCloseButton = addCardModal.querySelector('.popup__close-button');
-let editProfileCloseButton = editProfileModal.querySelector('.popup__close-button');
+const addCardCloseButton = addCardModal.querySelector('.popup__close-button');
+const editProfileCloseButton = editProfileModal.querySelector('.popup__close-button');
 
-let submitPopupButton = document.querySelector('.popup__submit-button');
+const submitPopupButton = document.querySelector('.popup__submit-button');
 
-
-let profileName = document.querySelector('.profile__name');
-let profileDesc = document.querySelector('.profile__description');
+const profileName = document.querySelector('.profile__name');
+const profileDesc = document.querySelector('.profile__description');
 
 // Form data
-let nameInput = editForm.querySelector('.popup__field_type_name');
-let jobInput = editForm.querySelector('.popup__field_type_desc');
+const nameInput = editForm.querySelector('.popup__field_type_name');
+const jobInput = editForm.querySelector('.popup__field_type_desc');
 
-let placeInput = addCardForm.querySelector('.popup__field_type_place');
-let urlInput = addCardForm.querySelector('.popup__field_type_url');
+const placeInput = addCardForm.querySelector('.popup__field_type_place');
+const urlInput = addCardForm.querySelector('.popup__field_type_url');
 
 const imageModalTitle = imageModal.querySelector('.popup__title');
 const imageModalImg = imageModal.querySelector('.popup__image');
